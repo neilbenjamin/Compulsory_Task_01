6e35ad53b961e4234f61d3a5ff30d378e8af89d6
neilbenjamin <neilbenjaminmusic@gmail.com>
2023-08-28 21:58:31 +0200

    This is the original code

---
+++ gitTask.js
@@ -0,0 +1,11 @@
+const userName = prompt("What is your name?")
+const addUserName = () => {
+    console.log(`Hi ${userName}`)
+}
+addUserName()
+
+const userAge = prompt("What is your age?")
+const checkUserAge = () => {
+    console.log(`${userName} is ${userAge} old.`)
+}
+checkUserAge()
